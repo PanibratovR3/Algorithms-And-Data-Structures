cfaa6a738cb4d41434f62d22eb4eddc0fdcc9cbf
PanibratovR3 <roman.panibratov@gmail.com>
2025-06-06 17:27:23 +0300

    Implemented iterative version of Fibbonacci sequence.

--- Recursion/fibonacci.js
+++ Recursion/fibonacci.js
@@ -1,19 +1,28 @@
-function fibbonacci(n) {
-  if (n <= 1) return n;
-  return fibbonacci(n - 1) + fibbonacci(n - 2);
-}
-
-function fibbonacciArray(n) {
+function fibs(n) {
   if (n === 0) {
     return [0];
   } else if (n === 1) {
     return [0, 1];
   } else {
-    let fibs = fibbonacciArray(n - 1);
+    let sequence = [0, 1];
+    for (let i = 2; i < n; i++) {
+      sequence.push(sequence[i - 1] + sequence[i - 2]);
+    }
+    return sequence;
+  }
+}
+
+function fibsRec(n) {
+  if (n === 0) {
+    return [0];
+  } else if (n === 1) {
+    return [0, 1];
+  } else {
+    let fibs = fibsRec(n - 1);
     fibs.push(fibs[fibs.length - 1] + fibs[fibs.length - 2]);
     return fibs;
   }
 }
 
-console.log(fibbonacci(7));
-console.log(fibbonacciArray(7));
+console.log(fibs(8));
+console.log(fibsRec(8));
