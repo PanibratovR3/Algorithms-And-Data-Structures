24bff942384bc7dad9c332852b16f7f1b3a2ddfd
PanibratovR3 <roman.panibratov@gmail.com>
2025-06-13 12:28:51 +0300

    Created class of ChessNode, array of possible knight's moves ,chessboard size variable.

--- KnightTravails/knightTravails.js
+++ KnightTravails/knightTravails.js
@@ -15,3 +15,25 @@ class Queue {
     return this.items.length === 0;
   }
 }
+
+class ChessNode {
+  constructor(position) {
+    this.position = position;
+    this.parent = null;
+    this.distance = null;
+  }
+}
+
+const possibleMoves = [
+  [2, 1],
+  [2, -1],
+  [-2, 1],
+  [-2, -1],
+  [1, 2],
+  [-1, 2],
+  [1, -2],
+  [-1, -2],
+];
+
+const SIZE = 8;
+let queue = new Queue();
